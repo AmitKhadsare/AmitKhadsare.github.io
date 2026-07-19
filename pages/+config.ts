68ae06fb3cb8f3e13bab9958344458b0fd3bb21e
AmitKhadsare <amitkhadsare@gmail.com>
2026-07-19 10:44:13 +0530

    Configure Vike to support partial pre-rendering to suppress non-static route build warnings

--- pages/+config.ts
+++ pages/+config.ts
@@ -2,5 +2,7 @@ import type { Config } from 'vike/types';
 
 // Minimal config - prerendering is enabled globally
 export default {
-    prerender: true
+    prerender: {
+        partial: true
+    }
 } satisfies Config;
